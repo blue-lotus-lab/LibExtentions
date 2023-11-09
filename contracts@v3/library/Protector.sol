562dc85c01ecabf588bf39d1e57f0cf242c87568
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-11-09 23:39:10 +0330

    Create Protector.sol

---
+++ contracts@v3/library/Protector.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @dev How to use:
+ * first import library, then for example
+ *  ```
+ *    function fx() public view returns (bool) {
+ *        return LibProtector._protected();
+ *    }
+ *  
+ *    // reentrancy guard
+ *    function withdraw() public payable {
+ *        require(LibProtector._protected());
+ *        // the code here
+ *    }
+ *  ```
+ * then implementing requirment functions.
+ * security: using checks in functions to ensure about the user was added or removed before. 
+ * in this case can to using "onlyOwner" or user role.
+ *
+ * Library created by: https://lotuschain.org {LibExtentions}
+*/
+
+library LibProtector {
+    // ---------------------------------------------------------------
+    // • for library usecase
+    // example use:
+    // require(LibProtector._protected());
+    // --------------------------------------------------------------- 
+    function _protected() internal view returns (bool) {
+        if(isContract(msg.sender)) {
+            return false;
+        }
+        return true;
+    }
+
+    // •——• utils
+    function isContract(address account) internal view returns (bool) {
+        uint size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
+}
+
+/* ----------------------------------------------------------------------------------
+[
+    { 
+        "creator": "lotuschain-lab",
+        "web3-home": "https://lotuschain.org",
+        "ALT-id": "@ALT"
+    }
+]
+---------------------------------------------------------------------------------- */
